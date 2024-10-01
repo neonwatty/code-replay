e902a37643ca0642714dd46fc248e5ee4067bbcb
Jeremy Watt <jermwatt@gmail.com>
2024-10-01 06:19:02 -0700

    add timer to recorder

--- record.js
+++ record.js
@@ -4,6 +4,37 @@ const logArea = document.getElementById("log");
 const startButton = document.getElementById("record-start");
 const stopButton = document.getElementById("record-stop");
 const clearButton = document.getElementById("record-clear");
+const timer = document.getElementById("timer");
+
+// timer
+let counter = 0;
+let interval;
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function convertSec(cnt) {
+  let sec = cnt % 60;
+  let min = Math.floor(cnt / 60);
+  if (sec < 10) {
+    if (min < 10) {
+      return "0" + min + ":0" + sec;
+    } else {
+      return min + ":0" + sec;
+    }
+  } else if (min < 10 && sec >= 10) {
+    return "0" + min + ":" + sec;
+  } else {
+    return min + ":" + sec;
+  }
+}
+
+function startTimer() {
+  interval = setInterval(function () {
+    timer.innerHTML = convertSec(counter++); // timer start counting here...
+  }, 1000);
+}
 
 // Array to store keystrokes
 let keystrokes = [];
@@ -34,6 +65,7 @@ function recorder(event) {
 
 // Event listener for keydown events
 function startRecorder() {
+  // start recorder
   inputArea.addEventListener("keydown", (event) => {
     recorder(event);
   });
@@ -48,12 +80,19 @@ startButton.addEventListener("click", () => {
   // clear keystrokes
   keystrokes = [];
 
+  // start timer
+  startTimer();
+
   // start recorder
   startRecorder();
 });
 
 // stop recording
 stopButton.addEventListener("click", () => {
+  // stop timer
+  stopTimer();
+
+  // stop keydown event listener
   inputArea.removeEventListener("keydown", recorder);
 });
 
@@ -64,6 +103,9 @@ clearButton.addEventListener("click", () => {
 
   // clear out input html
   inputArea.value = "";
+
+  // reset timer
+  timer.innerHTML = "00:00";
 });
 
 export { keystrokes };
